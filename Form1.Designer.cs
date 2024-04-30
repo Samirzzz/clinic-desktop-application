6ca5f374188caf7966db4779f6fe3f053f02300d
Samirzzz <abdelrahman.200300@gmail.com>
2024-04-30 23:45:18 +0300

    add patients

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            namelbl = new Label();
+            num = new Label();
+            patient_name = new TextBox();
+            patient_number = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // namelbl
+            // 
+            namelbl.AutoSize = true;
+            namelbl.BackColor = Color.Transparent;
+            namelbl.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            namelbl.Location = new Point(321, 157);
+            namelbl.Name = "namelbl";
+            namelbl.Size = new Size(62, 25);
+            namelbl.TabIndex = 0;
+            namelbl.Text = "Name";
+            namelbl.Click += label1_Click;
+            // 
+            // num
+            // 
+            num.AutoSize = true;
+            num.BackColor = Color.Transparent;
+            num.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num.Location = new Point(302, 226);
+            num.Name = "num";
+            num.Size = new Size(81, 25);
+            num.TabIndex = 1;
+            num.Text = "Number";
+            // 
+            // patient_name
+            // 
+            patient_name.Location = new Point(389, 155);
+            patient_name.Name = "patient_name";
+            patient_name.Size = new Size(402, 27);
+            patient_name.TabIndex = 2;
+            patient_name.TextChanged += textBox1_TextChanged;
+            // 
+            // patient_number
+            // 
+            patient_number.Location = new Point(389, 227);
+            patient_number.Name = "patient_number";
+            patient_number.Size = new Size(402, 27);
+            patient_number.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(513, 297);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 33);
+            button1.TabIndex = 4;
+            button1.Text = "save";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(436, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(299, 41);
+            label3.TabIndex = 5;
+            label3.Text = "Patient Registration";
+            label3.Click += label3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GrayText;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1121, 450);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(patient_number);
+            Controls.Add(patient_name);
+            Controls.Add(num);
+            Controls.Add(namelbl);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label namelbl;
+        private Label num;
+        private TextBox patient_name;
+        private TextBox patient_number;
+        private Button button1;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,104 @@
+using MySql.Data.MySqlClient;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
 namespace clinic_system
 {
     public partial class Form1 : Form
     {
+        public class db
+        {
+           public string mysqlconn = "server=localhost; user=root; database=clinic-system; password=";
+           public MySqlConnection mysqlconnection;
+            public void connection()
+            {
+                string mysqlconn = "server=localhost; user=root; database=clinic-system; password=";
+                mysqlconnection = new MySqlConnection(mysqlconn);
+                try
+                {
+                    mysqlconnection.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+                
+
+            }
+
+            public void ExecuteNonQuery(string query)
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand(query, mysqlconnection);
+                mySqlCommand.ExecuteNonQuery();
+            }
+        }
+        public class Patient
+        {
+            public int pid;
+            public string name;
+            public string number;
+            db d = new db();
+          
+
+        }
+        public class Doctor
+        {
+            public int did;
+            public string drname;
+            public int drnumber;
+            public string spec;
+
+        }
+        private db dbInstance;
+        private Patient patientInstance;
         public Form1()
         {
             InitializeComponent();
+            dbInstance = new db();
+            patientInstance = new Patient();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dbInstance.connection();
+            patientInstance.name = patient_name.Text.ToString();
+            patientInstance.number = patient_number.Text.ToString();
+            string query = "INSERT INTO patient (name, number) VALUES (@name, @number)";
+            MySqlCommand mySqlCommand = new MySqlCommand(query, dbInstance.mysqlconnection);
+            mySqlCommand.Parameters.AddWithValue("@name", patientInstance.name);
+            mySqlCommand.Parameters.AddWithValue("@number", patientInstance.number);
+            int rowsAffected = mySqlCommand.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+            {
+                MessageBox.Show("User added successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Failed to add user.");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
